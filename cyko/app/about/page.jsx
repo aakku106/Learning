ab93da2d9329f45f3190f76f61fdf7a11918e74e
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-10-01 17:12:46 +0545

    Add new pages and update layout metadata; remove unused SVG files

---
+++ cyko/app/about/page.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+function page() {
+  return <div>this is about page</div>;
+}
+
+export default page;
---
+++ cyko/app/portfolio/page.jsx
@@ -0,0 +1,7 @@
+/** @format */
+
+import React from "react";
+
+export default function page() {
+  return <div>this is my poetfolio page</div>;
+}
