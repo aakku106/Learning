ac791ba539471a9007ebd7615a6353abf518db5e
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-10-01 23:55:58 +0545

    Refactor Nav component to use Link for navigation instead of anchor tags

--- cyko/app/components/Nav.jsx
+++ cyko/app/components/Nav.jsx
@@ -7,13 +7,8 @@ export default function Nav() {
   return (
     <>
       <nav className="flex gap-4 p-4 bg-slate-200">
-        <a href="/">Home</a>
         <Link href="/">Home</Link>
-
-        <a href="/count">Count</a>
         <Link href="/count">Count</Link>
-
-        <a href="/about">About</a>
         <Link href="/about">About</Link>
       </nav>
     </>
