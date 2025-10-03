f694a8a0a96fa78fbeaa8305740fddaad4ae71a9
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-10-03 16:16:06 +0545

    main intry page for shoping

--- cyko/app/store/page.jsx
+++ cyko/app/store/page.jsx
@@ -1,9 +1,26 @@
 /** @format */
 
 import React from "react";
+import "./pageStyle.css";
+import Fashion from "./Fashion";
 
 const page = () => {
-  return <h1>Shopping App </h1>;
+  return (
+    <>
+      <h1>Shopping App </h1>
+      <section>
+        <div>
+          <h2>Automobiles</h2>
+        </div>
+        <div>
+          <h2>Electronics</h2>
+        </div>
+        <div>
+          <h2>Fashion</h2>
+          <Fashion />
+        </div>
+      </section>
+    </>
+  );
 };
-
 export default page;
